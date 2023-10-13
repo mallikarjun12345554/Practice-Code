1ea73ba8dcab65cdfb1d6f91757189c16247a3ff
mallikarjun1234441 <118536667+mallikarjun1234441@users.noreply.github.com>
2023-10-13 20:37:10 +0530

    19-StringBuffers

---
+++ 19-StringBuffers_StringBuilders/src/com/dl/one/Eg1.java
@@ -0,0 +1,11 @@
+package com.dl.one;
+
+public class Eg1 {
+
+	public static void main(String[] args) {
+		StringBuffer buffer = new StringBuffer();
+		StringBuilder builder = new StringBuilder();
+
+	}
+
+}
